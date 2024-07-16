a2f0aaf5ef978fed9fb27753b9151d1953390d93
Cale <caleworkman@gmail.com>
2024-07-16 00:31:58 -0500

    first

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker'
+import { useState } from 'react';
+
+import data from './assets/formulas.json';
 
 export default function App() {
+
+  const [selectedFormulaIdx, setSelectedFormulaIdx] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: '#fff' }}>Select a Formula</Text>
       <StatusBar style="auto" />
+
+      <Picker selectedValue={selectedFormulaIdx} onValueChange={idx => setSelectedFormulaIdx(idx)}>
+        {data.formulas.map((x, idx) => <Picker.Item key={x.uuid} label={x.name} value={idx} />)}
+      </Picker>
+
+      <p>
+        {data.formulas[selectedFormulaIdx]?.name}
+      </p>
+
     </View>
   );
 }
@@ -13,7 +29,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
